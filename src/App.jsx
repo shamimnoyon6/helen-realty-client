1d8af26b122e11be4516ce8d53a2c3a1eb35146c
shamimnoyon6 <shamimnoyon6@gmail.com>
2024-02-03 12:04:23 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>hello</h1>
+        <button class="btn btn-primary">Primary</button>
+        <button class="btn btn-secondary">Secondary</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
