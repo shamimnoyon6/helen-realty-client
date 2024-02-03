3a2c5f7db82601af586442a1d12c8b521ad6ca8d
shamimnoyon6 <shamimnoyon6@gmail.com>
2024-02-03 12:58:37 +0600

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -11,40 +11,23 @@ function App() {
               role="button"
               className="btn btn-ghost btn-circle"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h7"
-                />
-              </svg>
+              <img
+                src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpngtree.com%2Fso%2Fhouse-logo&psig=AOvVaw2KoUlTG_43tnS38RCygT-v&ust=1707028973473000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKjszNDJjoQDFQAAAAAdAAAAABAE"
+                alt=""
+              />
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <a>Homepage</a>
-              </li>
-              <li>
-                <a>Portfolio</a>
-              </li>
-              <li>
-                <a>About</a>
-              </li>
-            </ul>
+            
           </div>
         </div>
         <div className="navbar-center">
-          <a className="btn btn-ghost text-xl">HELEN REALTY</a>
+          <a className="btn btn-ghost text-xl">Home</a>
+          <a className="btn btn-ghost text-xl">Buy</a>
+          <a className="btn btn-ghost text-xl">Sell</a>
+          <a className="btn btn-ghost text-xl">Rent</a>
+          <a className="btn btn-ghost text-xl">About Us</a>
+          <a className="btn btn-ghost text-xl">Contact us</a>
         </div>
+
         <div className="navbar-end">
           <button className="btn btn-ghost btn-circle">
             <svg
